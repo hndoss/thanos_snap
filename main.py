9b10e562e51b9a68026758cef2270d7c70af4342
Hector Rosales <hector.rosales@acklenavenue.com>
2020-03-20 16:02:33 -0600

    working remote destroy.

--- main.py
+++ main.py
@@ -24,26 +24,33 @@ def parse_arguments():
     args = parser.parse_args()
     print(args.accumulate(args.integers))
 
+def destroy_local_environment(tf):
+    tf.init(capture_output=False)
+
+def destroy_remote_environment(tf):
+    tf.init(capture_output=False, backend_config={'key': 'falcon/staging-frontend' })
+
 def main():
     print('testing')
-    parse_arguments()
-    os.chdir('local/')
-
+    # parse_arguments()
+    os.chdir('remote/')
     cwd = os.getcwd()
-    print(cwd)
     tf = Terraform(working_dir=cwd)
-    tf.init(capture_output=False)
+    destroy_local_environment(tf)
+    destroy_remote_environment(tf)
+
+    tf.init(capture_output=False, backend_config={'key': 'falcon/staging-frontend' }) #remote
+    # tf.init(capture_output=False) local
+    
+    tf.destroy(capture_output=False)
     # tf.destroy(capture_output=False, var={'access_key': '', 'secret_key': '', 'aws_region': ''})
-    dir_path = cwd+'/.terraform'
-    try:
-        shutil.rmtree(dir_path)
-    except OSError as e:
-        print("Error: %s : %s" % (dir_path, e.strerror))
-    os.remove('terraform.tfstate')
+    # dir_path = cwd+'/.terraform'
+    # try:
+    #     shutil.rmtree(dir_path)
+    # except OSError as e:
+    #     print("Error: %s : %s" % (dir_path, e.strerror))
+    # os.remove('terraform.tfstate')
+
 
 
 main()
-
-
-  terraform apply -var='a=b' -var='c=d'
-  --> tf.apply(var={'a':'b', 'c':'d'})
